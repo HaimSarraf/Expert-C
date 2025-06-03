fd2b836aae5b63160161db99077c7db605106d5a
Haim Sarraf <sarhangaurelianobuendia@yahoo.com>
2025-06-03 12:21:35 +0330

    ansi-k&r

---
+++ ansi-vs-k&r-2/main.c
@@ -0,0 +1,40 @@
+#include <stddef.h>
+#include <stdio.h>
+#include <string.h>
+
+int array[] = {23, 34, 12, 17, 204, 99, 16};
+#define TOTAL_ELEMENTS (sizeof(array) / sizeof(array[0]))
+
+#define type_of(x)                                                             \
+  _Generic((x),                                                                \
+      unsigned long: "unsigned long",                                          \
+      unsigned int: "unsigned int",                                            \
+      int: "int",                                                              \
+      default: "unknown")
+
+int main() {
+  int d = -1, x;
+
+  size_t total = TOTAL_ELEMENTS;
+
+  printf("Type of TOTAL_ELEMENT is %s\n", type_of(total));
+
+  // the line below will occur some problems in complition
+  //    if (d <= TOTAL_ELEMENTS - 2)
+
+  // it would be fixed just by adding an int keyword immediately before
+  // TOTAL_ELEMENT
+  if (d <= (int)TOTAL_ELEMENTS - 2) {
+    x = array[d + 1];
+  }
+
+  printf("%d\n", x);
+
+  if (strcmp(type_of(total), type_of(d)) == 0) {
+    printf("Types are Compatible\n");
+  } else {
+    printf("Types are NOT Compatible\n");
+  }
+
+  return 0;
+}
